8dc6fdb2503f88ca0dd1522f24b8e7841d611a25
Tonga98 <gaston.pojma@hotmail.com>
2024-11-28 18:19:51 -0300

    Primer commit

---
+++ programaPojmaevichMartin.php
@@ -0,0 +1,91 @@
+<?php
+include_once("wordix.php");
+
+
+
+/**************************************/
+/***** DATOS DE LOS INTEGRANTES *******/
+/**************************************/
+
+/*
+Apellido: Pojmaevich
+Nombre: Gaston
+Legajo FAI-2856
+Carrera: TDW
+Mail: gaston.pojma@hotmail.com
+github: github.com/Tonga98
+
+Apellido: Martin
+Nombre: Gaston
+Legajo FAI-
+Carrera: TDW
+Mail:
+github: github.com/
+*/
+
+
+/**************************************/
+/***** DEFINICION DE FUNCIONES ********/
+/**************************************/
+
+/**
+ * Obtiene una colección de palabras
+ * @return array
+ */
+function cargarColeccionPalabras()
+{
+    $coleccionPalabras = [
+        "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
+        "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
+        /* Agregar 5 palabras más */
+    ];
+
+    return ($coleccionPalabras);
+}
+
+/* ****COMPLETAR***** */
+
+
+
+/**************************************/
+/*********** PROGRAMA PRINCIPAL *******/
+/**************************************/
+
+//Declaración de variables:
+
+
+//Inicialización de variables:
+
+
+//Proceso:
+
+$partida = jugarWordix("MELON", strtolower("MaJo"));
+//print_r($partida);
+//imprimirResultado($partida);
+
+
+
+/*
+do {
+    $opcion = ...;
+
+    
+    switch ($opcion) {
+        case 1: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+
+            break;
+        case 2: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
+
+            break;
+        case 3: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+
+            break;
+        
+            //...
+    }
+} while ($opcion != X);
+*/
